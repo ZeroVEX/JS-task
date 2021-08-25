3c518c44af5fc00604b66faf88f27489682a8114
Алексей Тручек <truchek99@mail.ru>
2021-08-25 07:40:29 +0300

    Working on task 2

---
+++ DateConverter.js
@@ -0,0 +1,46 @@
+const formats = {
+    "DDMMYYYY" : /^(?<day>0[1-9]|[1-2][0-9]|3[0-1])(?<month>0[1-9]|1[0-2])(?<year>[0-9]{4})$/,
+    "MMDDYYYY" : /^(?<month>0[1-9]|1[0-2])(?<day>0[1-9]|[1-2][0-9]|3[0-1])(?<year>[0-9]{4})$/,
+    "YYYYDDMM" : /^(?<year>[0-9]{4})(?<day>0[1-9]|[1-2][0-9]|3[0-1])(?<month>0[1-9]|1[0-2])$/,
+    "YYYYMMDD" : /^(?<year>[0-9]{4})(?<month>0[1-9]|1[0-2])(?<day>0[1-9]|[1-2][0-9]|3[0-1])$/,
+    "DD-MM-YYYY" : /^(?<day>0[1-9]|[1-2][0-9]|3[0-1])-(?<month>0[1-9]|1[0-2])-(?<year>[0-9]{4})$/,
+    "MM-DD-YYYY" : /^(?<month>0[1-9]|1[0-2])-(?<day>0[1-9]|[1-2][0-9]|3[0-1])-(?<year>[0-9]{4})$/,
+    "YYYY-DD-MM" : /^(?<year>[0-9]{4})-(?<day>0[1-9]|[1-2][0-9]|3[0-1])-(?<month>0[1-9]|1[0-2])$/,
+    "YYYY-MM-DD" : /^(?<year>[0-9]{4})-(?<month>0[1-9]|1[0-2])-(?<day>0[1-9]|[1-2][0-9]|3[0-1])$/,
+    "DD/MM/YYYY" : /^(?<day>0[1-9]|[1-2][0-9]|3[0-1])\/(?<month>0[1-9]|1[0-2])\/(?<year>[0-9]{4})$/,
+    "MM/DD/YYYY" : /^(?<month>0[1-9]|1[0-2])\/(?<day>0[1-9]|[1-2][0-9]|3[0-1])\/(?<year>[0-9]{4})$/,
+    "YYYY/DD/MM" : /^(?<year>[0-9]{4})\/(?<day>0[1-9]|[1-2][0-9]|3[0-1])\/(?<month>0[1-9]|1[0-2])$/,
+    "YYYY/MM/DD" : /^(?<year>[0-9]{4})\/(?<month>0[1-9]|1[0-2])\/(?<day>0[1-9]|[1-2][0-9]|3[0-1])$/,
+}
+
+function DateConverter() {
+
+}
+
+DateConverter.prototype.convert = function(date, fromFormat, toFormat) {
+    let regex = formats[fromFormat];
+    if (regex === undefined) {
+        return "From format is not supported"
+    }
+    if (formats[toFormat] === undefined) {
+        return "To format is not supported"
+    }
+    else if (!regex.test(date)) {
+        return "Date does not match format"
+    }
+    else {
+        let parsedDate = parseDate(date, regex);
+        let convertedDate = buildNewDate(parsedDate, toFormat);
+        return convertedDate;
+    }
+};
+
+function parseDate(date, regex) {
+    let parsedDate = date.match(regex).groups;
+    return parsedDate;
+}
+
+function buildNewDate(parsedDate, toFormat) {
+    let newDate = toFormat.replace("MM", parsedDate.month).replace("DD", parsedDate.day).replace("YYYY", parsedDate.year);
+    return newDate;
+}
---
+++ Testing.js
@@ -0,0 +1,11 @@
+let converter = new DateConverter();
+let convertedDate = converter.convert("12319999", "MMDDYYYY", "MM/DD/YYYY");
+console.log(convertedDate);
+convertedDate = converter.convert("31129999", "DDMMYYYY", "YYYY-DD-MM");
+console.log(convertedDate);
+convertedDate = converter.convert("9999/12/31", "YYYY/MM/DD", "YYYYDDMM");
+console.log(convertedDate);
+convertedDate = converter.convert("12-12-9999", "MMDDYY", "MM/DD/YYYY");
+console.log(convertedDate);
+convertedDate = converter.convert("12-12-9999", "MM/DD/YYYY", "MM/DD/YYYY");
+console.log(convertedDate);
